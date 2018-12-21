fc8f38cc22f6b2ea208d8c420d1c7fbeb2c1582d
David Lares <david.e.lares@gmail.com>
2018-12-21 14:54:41 -0400

    WebSocket funcionality Rails View and Go Server

--- server.go
+++ server.go
@@ -4,6 +4,9 @@ import (
   "fmt"
   "gopkg.in/redis.v3"
   "encoding/json"
+  "github.com/gorilla/mux"
+  "github.com/gorilla/websocket"
+  "net/http"
 )
 
 type Request struct {
@@ -11,7 +14,15 @@ type Request struct {
   Name string
 }
 
-func ConnectNewClient(){
+type Client struct {
+  Id int
+  websocket *websocket.Conn
+}
+
+// store Client structs for bidi communications
+var Clients = make(map[int] Client)
+
+func ConnectNewClient(channel_request chan Request){
   // redis client
   client := redis.NewClient(&redis.Options{
     Addr: "localhost:6379",
@@ -30,21 +41,74 @@ func ConnectNewClient(){
     if err != nil {
       fmt.Println("Unable to read message")
     }
-    
+
     request := Request{}
     if err := json.Unmarshal([]byte(message.Payload), &request); err != nil {
       fmt.Println("Unable to read JSON")
     }
     fmt.Println(request.Id)
     fmt.Println(request.Name)
+
+    // getting data
+    channel_request <- request
+
     // fmt.Println(message.Channel)
     // fmt.Println(message.Payload)
   }
-
   fmt.Println("Connection stablished")
+}
+
+func Subscribe(w http.ResponseWriter, r *http.Request){
+  ws, err := websocket.Upgrade(w,r,nil,1024,1024)
+  if err != nil {
+    return
+  }
+  fmt.Println("New Websocket")
+  // counting clients
+  count := len(Clients)
+  // adding clients
+  new_client := Client{count, ws}
+  Clients[count] = new_client
+  fmt.Println("New client")
+  for {
+    _,_,err := ws.ReadMessage()
+    if err != nil {
+      delete(Clients, new_client.Id)
+      fmt.Println("Client gone")
+      return
+    }
+  }
+}
+
+func ValidateChannel(request chan Request){
+  for {
+    select{
+    case r := <- request:
+      // send messages
+      SendMessage(r)
+    }
+  }
+}
+
+func SendMessage(request Request){
+  for _, client := range Clients{
+    if err := client.websocket.WriteJSON(request); err != nil {
+      return
+    }
+  }
 }
 
 func main(){
   fmt.Println("Hello, World")
-  ConnectNewClient()
+  // channel request
+  channel_request := make(chan Request)
+  go ValidateChannel(channel_request)
+  // go Routing
+  go ConnectNewClient(channel_request)
+  // Websocket server
+  mux := mux.NewRouter()
+  mux.HandleFunc("/subscribe", Subscribe).Methods("GET")
+  http.Handle("/", mux)
+  fmt.Println("Server running on 8000")
+  http.ListenAndServe(":8000", nil)
 }
